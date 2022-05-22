423f2b475a1045aab45fe791422143e47040974e
HugoFara <hugo.farajallah@protonmail.com>
2022-05-22 14:43:11 +0900

    Regenerated documentation.

--- docs/html/search/functions_10.js
+++ docs/html/search/functions_10.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['update_5fdatabase_0',['update_database',['../database__connect_8php.html#aee77a1d03a5cce20d89d97a88d587dd6',1,'database_connect.php']]],
-  ['update_5fword_5fstatus_1',['update_word_status',['../ajax__chg__term__status_8php.html#aeb875bfdb5f0c40515cd8e0d3a3490c7',1,'ajax_chg_term_status.php']]],
-  ['url_5fbase_2',['url_base',['../kernel__utility_8php.html#aaf8aa013b97e5bcf60c560fd6cad05fe',1,'kernel_utility.php']]]
+  ['update_5fdefault_5fvalues_1',['update_default_values',['../database__connect_8php.html#a31fb489225a5a5c071b6fb7d2ddcbea1',1,'database_connect.php']]],
+  ['update_5fword_5fstatus_2',['update_word_status',['../ajax__chg__term__status_8php.html#aeb875bfdb5f0c40515cd8e0d3a3490c7',1,'ajax_chg_term_status.php']]],
+  ['url_5fbase_3',['url_base',['../kernel__utility_8php.html#aaf8aa013b97e5bcf60c560fd6cad05fe',1,'kernel_utility.php']]]
 ];
--- docs/html/search/functions_b.js
+++ docs/html/search/functions_b.js
@@ -3,12 +3,14 @@ var searchData=
   ['pageend_0',['pageend',['../kernel__utility_8php.html#a46df200f3bda724d43075130dfcc24ba',1,'kernel_utility.php']]],
   ['pagestart_1',['pagestart',['../session__utility_8php.html#ad9e9a6e079198578de4a3fb8b2972bc1',1,'session_utility.php']]],
   ['pagestart_5fnobody_2',['pagestart_nobody',['../session__utility_8php.html#aa2ddde28f52334e0f6b8bfc1b89c12cb',1,'session_utility.php']]],
-  ['phonetic_5freading_3',['phonetic_reading',['../session__utility_8php.html#a1c05bfcd7542596ade6e8e0604a4c0a7',1,'session_utility.php']]],
-  ['prepare_5ftest_5farea_4',['prepare_test_area',['../do__test__test_8php.html#af2d52017b291ec92dd04c977a6291314',1,'do_test_test.php']]],
-  ['prepare_5ftest_5ffooter_5',['prepare_test_footer',['../do__test__test_8php.html#a5c6c344ac760375da097f1067b235fb8',1,'do_test_test.php']]],
-  ['preparestyle_6',['prepareStyle',['../do__text__text_8php.html#ae71caba9eee9a49733ebaa7e3c16a8ed',1,'do_text_text.php']]],
-  ['print_5ffeed_5fresult_7',['print_feed_result',['../ajax__load__feed_8php.html#a88b565ab6a89b31bf4e5d3d2bfb986a8',1,'ajax_load_feed.php']]],
-  ['print_5ffile_5fpath_8',['print_file_path',['../session__utility_8php.html#a0caab76499b93b3291b2c8b2490ea3da',1,'session_utility.php']]],
-  ['print_5fsimilar_5fterms_9',['print_similar_terms',['../simterms_8php.html#ae757838966b3688392e9ed28f6721a37',1,'simterms.php']]],
-  ['print_5fterm_5ftest_10',['print_term_test',['../do__test__test_8php.html#aa4dcc9f7101f95be23101b1faa93f777',1,'do_test_test.php']]]
+  ['parse_5fjapanese_5ftext_3',['parse_japanese_text',['../database__connect_8php.html#a3117a4f4d8cd548f39fd047aa7617a24',1,'database_connect.php']]],
+  ['parse_5fstandard_5ftext_4',['parse_standard_text',['../database__connect_8php.html#afe31d7ed63ac9d55dab490b56651c9bd',1,'database_connect.php']]],
+  ['prepare_5ftest_5farea_5',['prepare_test_area',['../do__test__test_8php.html#af2d52017b291ec92dd04c977a6291314',1,'do_test_test.php']]],
+  ['prepare_5ftest_5ffooter_6',['prepare_test_footer',['../do__test__test_8php.html#a5c6c344ac760375da097f1067b235fb8',1,'do_test_test.php']]],
+  ['prepare_5ftext_5fparsing_7',['prepare_text_parsing',['../database__connect_8php.html#ae98a7593f72474bea963bdd31ef9ae5e',1,'database_connect.php']]],
+  ['preparestyle_8',['prepareStyle',['../do__text__text_8php.html#ae71caba9eee9a49733ebaa7e3c16a8ed',1,'do_text_text.php']]],
+  ['print_5ffeed_5fresult_9',['print_feed_result',['../ajax__load__feed_8php.html#a88b565ab6a89b31bf4e5d3d2bfb986a8',1,'ajax_load_feed.php']]],
+  ['print_5ffile_5fpath_10',['print_file_path',['../session__utility_8php.html#a0caab76499b93b3291b2c8b2490ea3da',1,'session_utility.php']]],
+  ['print_5fsimilar_5fterms_11',['print_similar_terms',['../simterms_8php.html#ae757838966b3688392e9ed28f6721a37',1,'simterms.php']]],
+  ['print_5fterm_5ftest_12',['print_term_test',['../do__test__test_8php.html#aa4dcc9f7101f95be23101b1faa93f777',1,'do_test_test.php']]]
 ];
