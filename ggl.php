32f0bd84001a7d810d9cc7267716f67df78c29cc
HugoFara <hugo.farajallah@protonmail.com>
2023-02-22 21:55:21 +0100

    Functional refactoring layer 2.

--- ggl.php
+++ ggl.php
@@ -8,7 +8,7 @@ require_once 'inc/classes/googleTranslateClass.php';
 
 use GoogleTranslate;
 
-function translate_term($text, $sl, $tl)
+function translate_text($text, $sl, $tl, $sentence_mode)
 {
     $file = GoogleTranslate::staticTranslate($text, $sl, $tl, getGoogleTimeToken());
 
@@ -20,15 +20,21 @@ function translate_term($text, $sl, $tl)
         " more..."
     );
 
-    if (!isset($_GET['sent'])) {
-        echo '<h3>Google Translate:  &nbsp; ' . 
-        '<span class="red2" id="textToSpeak" style="cursor:pointer" ' . 
-        'title="Click on expression for pronunciation">' . tohtml($text) . '</span> ' . 
-        '<img id="del_translation" src="icn/broom.png" style="cursor:pointer" ' . 
-        'title="Empty Translation Field" onclick="deleteTranslation ();"></img></h3>' . 
-        '<p>(Click on <img src="icn/tick-button.png" title="Choose" alt="Choose" /> ' .
-        'to copy word(s) into above term)<br />&nbsp;</p>';
+    if (!$sentence_mode) {
         ?>
+    <h3>Google Translate:  &nbsp; 
+        <span class="red2" id="textToSpeak" style="cursor:pointer" 
+        title="Click on expression for pronunciation">
+            <?php echo tohtml($text) ?>
+        </span> 
+        <img id="del_translation" src="icn/broom.png" style="cursor:pointer" 
+        title="Empty Translation Field" onclick="deleteTranslation ();"></img>
+    </h3>
+    <p>
+        (Click on <img src="icn/tick-button.png" title="Choose" alt="Choose" /> 
+        to copy word(s) into above term)<br />&nbsp;
+    </p>
+        
     <script type="text/javascript">
         //<![CDATA[
         $(document).ready( function() {
@@ -41,7 +47,10 @@ function translate_term($text, $sl, $tl)
         $('#textToSpeak').on('click', function () {
             const txt = $('#textToSpeak').text();
             const audio = new Audio();
-            audio.src = 'tts.php?tl=' + <?php echo json_encode($sl); ?> + '&q=' + txt;
+            audio.src = 'tts.php?' + $.param({
+                tl: <?php echo json_encode($sl); ?>, 
+                q: txt
+            });
             audio.play();
         });
         });
@@ -58,29 +67,43 @@ function translate_term($text, $sl, $tl)
             echo '<br />' . $gglink . "\n";
         }
 
-        echo '&nbsp;<hr />&nbsp;<form action="ggl.php" method="get">Unhappy?<br/>Change term: 
-	<input type="text" name="text" maxlength="250" size="15" value="' . tohtml($text) . '">
-	<input type="hidden" name="sl" value="' . tohtml($sl) . '">
-	<input type="hidden" name="tl" value="' . tohtml($tl) . '">
-	<input type="submit" value="Translate via Google Translate">
-	</form>';
+        ?>
+        &nbsp;<hr />&nbsp;
+        <form action="ggl.php" method="get">
+            Unhappy?<br/>Change term: 
+            <input type="text" name="text" maxlength="250" size="15" 
+            value="<?php echo tohtml($text); ?>">
+            <input type="hidden" name="sl" value="<?php echo tohtml($sl); ?>">
+            <input type="hidden" name="tl" value="<?php echo tohtml($tl); ?>">
+            <input type="submit" value="Translate via Google Translate">
+        </form>
+        <?php
     } else { 
-        echo '<h3>Sentence:</h3><span class="red2">' . tohtml($text) . 
-        '</span><br><br><h3>Google Translate:</h3>' . $gglink . 
-        '<br><table class="tab2" cellspacing="0" cellpadding="0">' . 
-        '<tr><td class="td1bot center" colspan="1">'. $file[0] . '</td></tr></table>'; 
+        ?>
+        <h3>Sentence:</h3>
+        <span class="red2"><?php echo tohtml($text); ?></span>
+        <br><br>
+        <h3>Google Translate:</h3>
+        <?php echo $gglink ?> 
+        <br>
+        <table class="tab2" cellspacing="0" cellpadding="0"> 
+            <tr>
+                <td class="td1bot center" colspan="1">
+                    <?php echo $file[0]; ?>
+                </td>
+            </tr>
+        </table>
+        <?php
     }
 }
 
 function do_content($text)
 {
-    $tl = $_GET["tl"];
-    $sl = $_GET["sl"];
     header('Pragma: no-cache');
     header('Expires: 0');
     pagestart_nobody('');
-    if (trim($text)!='') {
-        translate_term($text, $sl, $tl);
+    if (trim($text) != '') {
+        translate_text($text, $_GET["sl"], $_GET["tl"], isset($_GET['sent']));
     } else {
         echo "<p class=\"msgblue\">Term is not set!</p>";
     }
