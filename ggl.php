4c80873b5aa0b54963cb9da033cac9d7511fec85
HugoFara <hugo.farajallah@protonmail.com>
2023-02-22 22:26:06 +0100

    Documentation and small fixes.

--- ggl.php
+++ ggl.php
@@ -1,5 +1,19 @@
 <?php
 
+/*
+ * \file
+ * \brief Google Translate interface
+ * 
+ * Call: ggl.php?text=[text]&sl=[source language]&tl=[target language] ... translate text
+ *     ... sent=[int] ... single sentence mode.
+ * 
+ * @package Lwt
+ * @author  LWT Project <lwt-project@hotmail.com>
+ * @license Unlicense <http://unlicense.org/>
+ * @link    https://hugofara.github.io/lwt/docs/html/ggl_8php.html
+ * @since   1.6.0
+ * @since   2.7.0 Refactored with functional paradigm
+ */
 namespace Lwt\Ggl;
 
 require_once 'inc/session_utility.php';
@@ -8,6 +22,15 @@ require_once 'inc/classes/googleTranslateClass.php';
 
 use GoogleTranslate;
 
+/*
+ * Translate a single sentence using Google Translate.
+ * 
+ * @param string $text   Text to translate
+ * @param string $gglink Google Translate link
+ * @param array  $file   Array of translated terms, should contain 1 element
+ * 
+ * @return void
+ */
 function translate_sentence($text, $gglink, $file)
 {
     ?>
@@ -27,6 +50,17 @@ function translate_sentence($text, $gglink, $file)
     <?php
 }
 
+/*
+ * Translate input text using Google Translate.
+ * 
+ * @param string $text   Text to translate
+ * @param string $gglink Google Translate link
+ * @param array  $file   Array of translated terms 
+ * @param string $sl     Source language (e. g. "es")
+ * @param string $tl     Target language (e. g. "en")
+ * 
+ * @return void
+ */
 function translate_term($text, $gglink, $file, $sl, $tl)
 {
     ?>
@@ -44,7 +78,6 @@ function translate_term($text, $gglink, $file, $sl, $tl)
 </p>
     
 <script type="text/javascript">
-    //<![CDATA[
     $(document).ready( function() {
     let w = window.parent.frames['ro'];
     if (w === undefined) 
@@ -62,14 +95,13 @@ function translate_term($text, $gglink, $file, $sl, $tl)
         audio.play();
     });
     });
-
-    //]]>
 </script>
     <?php
     foreach ($file as $word){
-        echo '<span class="click" onclick="addTranslation(' . prepare_textdata_js($word) . ');">' . 
+        echo '<span class="click" onclick="addTranslation(' . 
+        prepare_textdata_js($word) . ');">' . 
         '<img src="icn/tick-button.png" title="Copy" alt="Copy" /> &nbsp; ' . 
-        $word . '</span><br />';
+        tohtml($word) . '</span><br />';
     }
     if (!empty($file)) {
         echo '<br />' . $gglink . "\n";
@@ -88,6 +120,16 @@ function translate_term($text, $gglink, $file, $sl, $tl)
     <?php
 }
 
+/*
+ * Translate input text using Google Translate.
+ * 
+ * @param string $text Text to translate
+ * @param string $sl Source language (e. g. "es")
+ * @param string $tl Target language (e. g. "en")
+ * @param bool $sentence_mode Set to true for full sentence translation
+ * 
+ * @return void
+ */
 function translate_text($text, $sl, $tl, $sentence_mode)
 {
     $file = GoogleTranslate::staticTranslate($text, $sl, $tl, getGoogleTimeToken());
@@ -107,6 +149,13 @@ function translate_text($text, $sl, $tl, $sentence_mode)
     }
 }
 
+/*
+ * Translate input text using Google Translate.
+ * 
+ * @param string $text Text to translate
+ * 
+ * @return void
+ */
 function do_content($text)
 {
     header('Pragma: no-cache');
