2cd6032af18c3e4a93bb7f5d561248d6f59162a5
HugoFara <Hugo.Farajallah@protonmail.com>
2023-07-06 11:21:50 +0900

    Linting, no code change.

--- inc/simterms.php
+++ inc/simterms.php
@@ -48,12 +48,13 @@ function wordLetterPairs($str): array
 /**
  * Similarity ranking of two UTF-8 strings $str1 and $str2
  * 
+ * Source http://www.catalysoft.com/articles/StrikeAMatch.html
+ * Source http://stackoverflow.com/questions/653157
+ * 
  * @return float SimilarityRanking
  */
 function getSimilarityRanking($str1, $str2) 
 {
-    // Source http://www.catalysoft.com/articles/StrikeAMatch.html
-    // Source http://stackoverflow.com/questions/653157
     $pairs1 = wordLetterPairs($str1);
     $pairs2 = wordLetterPairs($str2);
     $union = count($pairs1) + count($pairs2);
@@ -76,12 +77,11 @@ function getSimilarityRanking($str1, $str2)
 function get_similar_terms(
     $lang_id, $compared_term, $max_count, $min_ranking
 ): array { 
-
     global $tbpref;
     $compared_term_lc = mb_strtolower($compared_term, 'UTF-8');
-    $sql = "select WoID, WoTextLC from " . $tbpref . "words 
-    where WoLgID = " . $lang_id . 
-    " AND WoTextLC <> " . convert_string_to_sqlsyntax($compared_term_lc);
+    $sql = "SELECT WoID, WoTextLC FROM {$tbpref}words 
+    WHERE WoLgID = $lang_id
+    AND WoTextLC <> " . convert_string_to_sqlsyntax($compared_term_lc);
     $res = do_mysqli_query($sql);
     $termlsd = array();
     while ($record = mysqli_fetch_assoc($res)) {
@@ -121,37 +121,44 @@ function print_similar_terms($lang_id, $compared_term): string
     $termarr = get_similar_terms($lang_id, $compared_term, $max_count, 0.33);
     $rarr = array();
     foreach ($termarr as $termid) {
-        $sql = "select WoText, WoTranslation, WoRomanization from " . $tbpref . "words where WoID = " . $termid;
+        $sql = "SELECT WoText, WoTranslation, WoRomanization 
+        FROM {$tbpref}words WHERE WoID = $termid";
         $res = do_mysqli_query($sql);
         if ($record = mysqli_fetch_assoc($res)) {
             $term = tohtml($record["WoText"]);
             if (stripos($compare, $term) !== false) {
                 $term = '<span class="red3">' . $term . '</span>'; 
-            }
-            else {
-                $term = str_replace($compare, '<span class="red3"><u>' . $compare . '</u></span>', $term); 
+            } else {
+                $term = str_replace(
+                    $compare, 
+                    '<span class="red3"><u>' . $compare . '</u></span>', 
+                    $term
+                ); 
             }
             $tra = $record["WoTranslation"];
-            if ($tra == "*") { $tra = "???"; 
+            if ($tra == "*") { 
+                $tra = "???"; 
             }
             if (trim($record["WoRomanization"]) !== '') {
                 $romd = " [" . $record["WoRomanization"] . "]";
                 $rom = $record["WoRomanization"];
-            }
-            else {
+            } else {
                 $romd = "";
                 $rom = "";
             }
-            $rarr[] = '<img class="clickedit" src="icn/tick-button-small.png" title="Copy → Translation &amp; Romanization Field(s)" onclick="setTransRoman(' . prepare_textdata_js($tra) . ',' . prepare_textdata_js($rom) . ');" /> ' . $term . tohtml($romd) . ' — ' . tohtml($tra) . '<br />';
+            $rarr[] = '<img class="clickedit" src="icn/tick-button-small.png" ' .
+            'title="Copy → Translation &amp; Romanization Field(s)" ' .
+            'onclick="setTransRoman(' . 
+            prepare_textdata_js($tra) . ',' . prepare_textdata_js($rom) . ');" /> ' . 
+            $term . tohtml($romd) . ' — ' . tohtml($tra) . 
+            '<br />';
         }
         mysqli_free_result($res);
     }
-    if(count($rarr) == 0) {
+    if (count($rarr) == 0) {
         return "(none)"; 
     }
-    else {
-        return implode($rarr); 
-    }
+    return implode($rarr); 
 }
 
 // -------------------------------------------------------------
@@ -159,7 +166,10 @@ function print_similar_terms($lang_id, $compared_term): string
 function print_similar_terms_tabrow(): void 
 {
     if ((int)getSettingWithDefault("set-similar-terms-count") > 0) { 
-        echo '<tr><td class="td1 right">Similar<br />Terms:</td><td class="td1"><span id="simwords" class="smaller">&nbsp;</span></td></tr>'; 
+        echo '<tr>
+            <td class="td1 right">Similar<br />Terms:</td>
+            <td class="td1"><span id="simwords" class="smaller">&nbsp;</span></td>
+        </tr>'; 
     }
 } 
 
