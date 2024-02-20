ad0104c4a8c0134326c2c7d86cdce51bfb387eff
HugoFara <hugo.farajallah@protonmail.com>
2024-02-20 18:43:09 +0100

    Removes trailing whitespaces.

--- db/migrations/19700101_000001_lwt_original.sql
+++ db/migrations/19700101_000001_lwt_original.sql
@@ -1,59 +1,58 @@
 -- Migrations already present in the original LWT?
 
-ALTER TABLE words 
-    ADD WoTodayScore DOUBLE NOT NULL DEFAULT 0, 
-    ADD WoTomorrowScore DOUBLE NOT NULL DEFAULT 0, 
+ALTER TABLE words
+    ADD WoTodayScore DOUBLE NOT NULL DEFAULT 0,
+    ADD WoTomorrowScore DOUBLE NOT NULL DEFAULT 0,
     ADD WoRandom DOUBLE NOT NULL DEFAULT 0;
 
-ALTER TABLE words 
+ALTER TABLE words
     ADD WoWordCount tinyint(3) unsigned NOT NULL DEFAULT 0 AFTER WoSentence;
 
-ALTER TABLE words 
-    ADD INDEX WoTodayScore (WoTodayScore), 
-    ADD INDEX WoTomorrowScore (WoTomorrowScore), 
+ALTER TABLE words
+    ADD INDEX WoTodayScore (WoTodayScore),
+    ADD INDEX WoTomorrowScore (WoTomorrowScore),
     ADD INDEX WoRandom (WoRandom);
 
-ALTER TABLE languages 
+ALTER TABLE languages
     ADD LgRightToLeft tinyint(1) UNSIGNED NOT NULL DEFAULT 0;
 
-ALTER TABLE texts 
+ALTER TABLE texts
     ADD TxAnnotatedText LONGTEXT NOT NULL AFTER TxText;
 
-ALTER TABLE archivedtexts 
+ALTER TABLE archivedtexts
     ADD AtAnnotatedText LONGTEXT NOT NULL AFTER AtText;
 
-ALTER TABLE tags 
-    CHANGE TgComment TgComment VARCHAR(200) 
+ALTER TABLE tags
+    CHANGE TgComment TgComment VARCHAR(200)
     CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
 
-ALTER TABLE tags2 
-    CHANGE T2Comment T2Comment VARCHAR(200) 
+ALTER TABLE tags2
+    CHANGE T2Comment T2Comment VARCHAR(200)
     CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
 
-ALTER TABLE languages 
-    CHANGE LgGoogleTTSURI LgExportTemplate VARCHAR(1000) 
+ALTER TABLE languages
+    CHANGE LgGoogleTTSURI LgExportTemplate VARCHAR(1000)
     CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
 
-ALTER TABLE texts 
-    ADD TxSourceURI VARCHAR(1000) 
+ALTER TABLE texts
+    ADD TxSourceURI VARCHAR(1000)
     CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
 
-ALTER TABLE archivedtexts 
-    ADD AtSourceURI VARCHAR(1000) 
+ALTER TABLE archivedtexts
+    ADD AtSourceURI VARCHAR(1000)
     CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
 
-ALTER TABLE texts 
+ALTER TABLE texts
     ADD TxPosition smallint(5) NOT NULL DEFAULT 0;
 
-ALTER TABLE texts 
+ALTER TABLE texts
     ADD TxAudioPosition float NOT NULL DEFAULT 0;
 
-ALTER TABLE `wordtags` 
+ALTER TABLE `wordtags`
     DROP INDEX WtWoID;
 
-ALTER TABLE `texttags` 
+ALTER TABLE `texttags`
     DROP INDEX TtTxID;
 
-ALTER TABLE `archtexttags` 
+ALTER TABLE `archtexttags`
     DROP INDEX AgAtID;
-    
--- db/migrations/20240103_120316_2.10.0-fork.sql
+++ db/migrations/20240103_120316_2.10.0-fork.sql
@@ -1,16 +1,16 @@
--- LWT version 2.10.0 adds several features: 
+-- LWT version 2.10.0 adds several features:
 -- External voice api support, romanization hiding option, longer URI for text audio
 
 
 -- Add a third-party voice API
-ALTER TABLE `languages` 
+ALTER TABLE `languages`
     ADD COLUMN `LgTTSVoiceAPI` VARCHAR(2048) NOT NULL;
 
 -- Romanization disabling option
-ALTER TABLE `languages` 
+ALTER TABLE `languages`
     ADD COLUMN `LgShowRomanization` TINYINT(1) DEFAULT TRUE;
 
--- URI should be at least 2048 characters long: 
+-- URI should be at least 2048 characters long:
 -- https://stackoverflow.com/questions/417142/what-is-the-maximum-length-of-a-url-in-different-browsers
-ALTER TABLE `texts` 
+ALTER TABLE `texts`
     MODIFY COLUMN `TxAudioURI` VARCHAR(2048) NOT NULL;
